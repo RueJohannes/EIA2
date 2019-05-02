e027f38d3697b94943ae5af3e629e554510cda57
RueJohannes <derh4nn3s@gmail.com>
2019-05-02 14:50:21 +0200

    Update Server.ts
    Irgendwie gabs da eine Verwechslung mit der Server.ts Datei. Deshalb nochmal der Reupload.

--- Aufgaben/Aufgabe_6/Server.ts
+++ Aufgaben/Aufgabe_6/Server.ts
@@ -1,51 +1,28 @@
-function aufnahmeLMB(_event: Event): void {
-    if (_event.type == "click") {
-        karteAusgeben(1);
-    }
-}
-function aufnahmeLeertaste(_event: KeyboardEvent): void {
-    if (_event.keyCode == 32) {
-        karteAusgeben(1);
+import * as Http from "http"; //Ein Http Protokoll wird importiert
 
-    }
-}
-function handkartenAnzeigen(): void {
-    aufnahme = "";
-    for (let i: number = 0; i < hand.length; i++) 
-        aufnahme += `<div class="${hand[i].symbol}"> ${hand[i].symbol} ${hand[i].zahl} </div> `;
-    document.getElementById("Handkarten").innerHTML = aufnahme; 
-    for (let i: number = 0; i < hand.length; i++) {  
-        document.getElementById(hand[i].id.toString()).addEventListener("click", karteAblegen);
-    }
-    if (ablage.length > 0) {
-        let abgelegt: string = `<div class="${ablageStapel[ablageStapel.length - 1].farbe}" id="${ablageStapel[ablageStapel.length - 1].id}"> ${ablageStapel[ablageStapel.length - 1].farbe} ${ablageStapel[ablageStapel.length - 1].wert} </div> `;
-        document.getElementById("Ablage").innerHTML = abgelegt;
-    }
-}
-function karteAblegen(_event: Event): void {
-    let domCard: HTMLElement = <HTMLElement>_event.target;
-    console.log(domCard);
-    for (let i: number = 0; i < hand.length; i++) {
-        if (hand[i].id == Number(domCard.id)) {
-            console.log (hand[i].id);
-            ablageStapel.push(hand[i]);
-            hand.splice(i, 1);
-            kartenAnzeigen();
-        }
-    }
-}
-function handkartenSortieren (): void {
-    hand.sort(function (a: Karte, b: Karte ): number {
-        return a.id - b.id;        
-        }
-    );
-    kartenAnzeigen();
-}
-/*
-function handleKeydown(_event: Event): void {
-    if (_event. == )
-    console.log(_event);
-}
-*/
-kartenAnzahl();
+namespace L05_Server { //Code wird durch namespace gruppiert
+	console.log("Starting server"); //Auf der Console wird "Starting server" ausgegeben
+	let port: number = Number(process.env.PORT); //Variable port vom Typ number wird festgelegt
+	if (!port)
+		port = 8100; //Port wird auf 8100 festgelegt
+
+	let server: Http.Server = Http.createServer(); //Variable server vom Typ Http.Server wird mit neuem Server erstellt
+	server.addListener("request", handleRequest); //Dem Server wird ein Listener hinzugefügt der bei "request" die Funktion handleRequest ausführt
+	server.addListener("listening", handleListen); //Dem Server wird ein Listener hinzugefügt der bei "listening" die Funktion handleRequest ausführt
+	server.listen(port);
+
+	function handleListen(): void { //Funktion handleListen vom Typ void
+		console.log("Listening"); //Auf der Console wird "Listening" ausgegeben
+	}
+
+	function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void { // Funktion handleRequest vom Typ void wird durch die Parametern request und _response vom Typ Http.IncomingMessage und Http.ServerResponse dafür verantwortlich Anfragen anzunehmen und darauf zu reagieren
+		console.log("I hear voices!"); //Auf der Console wird "I hear voices!" ausgegeben
+
+		_response.setHeader("content-type", "text/html; charset=utf-8"); //Der Inhalt ("content-type", "text/html; charset=utf-8") wird bei _response im Header hinzugefügt
+		_response.setHeader("Access-Control-Allow-Origin", "*"); //Der Inhalt ("Access-Control-Allow-Origin", "*") wird bei _response im Header hinzugefügt
+
+		_response.write(_request.url); //der Inhalt wird an die URL gesendet
+
+		_response.end(); //_response wird beendet
+	}
 }
